a8b9f64e91905928adf79680a1f1e3aecc48d5d0
Bowie <carolinyr9@gmail.com>
2025-10-04 19:10:00 -0300

    🎉 begin a new project

---
+++ src/main/java/br/rocha/estocai/EstocaiApplication.java
@@ -0,0 +1,13 @@
+package br.rocha.estocai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstocaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstocaiApplication.class, args);
+	}
+
+}
