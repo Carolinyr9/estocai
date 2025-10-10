57aeb484d512ed5ab41475abd20bf461c66eb625
Bowie <carolinyr9@gmail.com>
2025-10-10 16:59:40 -0300

    🐛 fix routes names

--- src/main/java/br/rocha/estocai/controller/MovementController.java
+++ src/main/java/br/rocha/estocai/controller/MovementController.java
@@ -9,10 +9,12 @@ import br.rocha.estocai.model.dtos.MovementResponseDto;
 import br.rocha.estocai.service.MovementService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.data.domain.Page;
 
 
 @RestController
+@RequestMapping("/movements")
 public class MovementController {
     @Autowired
     MovementService movementService;
@@ -23,19 +25,19 @@ public class MovementController {
         return ResponseEntity.ok(movement);
     }
 
-    @GetMapping("/{type}")
+    @GetMapping("/type/{type}")
     public ResponseEntity<Page<MovementResponseDto>> getMovementsByType(Pageable pageable, @PathVariable String type){
         Page<MovementResponseDto> movement = movementService.getMovementsByType(type, pageable);
         return ResponseEntity.ok(movement);
     }
 
-    @GetMapping("/{description}")
+    @GetMapping("/description/{description}")
     public ResponseEntity<Page<MovementResponseDto>> getMovementsByDescription(Pageable pageable, @PathVariable String description){
-        Page<MovementResponseDto> movement = movementService.getMovementsByType(description, pageable);
+        Page<MovementResponseDto> movement = movementService.getMovementsByDescription(description, pageable);
         return ResponseEntity.ok(movement);
     }
 
-    @GetMapping("/{productId}")
+    @GetMapping("/product/{productId}")
     public ResponseEntity<Page<MovementResponseDto>> getMovementsByDProductId(Pageable pageable, @PathVariable Long productId){
         Page<MovementResponseDto> movement = movementService.getMovementsByProductId(productId, pageable);
         return ResponseEntity.ok(movement);
