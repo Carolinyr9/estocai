ae0ac227407664cf2749e6d3da9165af541b5601
Bowie <carolinyr9@gmail.com>
2025-10-17 14:33:07 -0300

    🎨 create test structure

--- src/main/java/br/rocha/estocai/model/dtos/CategoryResponseDto.java
+++ src/main/java/br/rocha/estocai/model/dtos/CategoryResponseDto.java
@@ -1,9 +1,5 @@
 package br.rocha.estocai.model.dtos;
 
-import java.util.List;
-
-import br.rocha.estocai.model.Product;
-
 public record CategoryResponseDto(Long id, String name, String description) {
     
 }
