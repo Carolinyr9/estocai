b4904805b1409129ab05d433d3164ffa4cbfa7d0
Bowie <carolinyr9@gmail.com>
2025-10-23 14:48:23 -0300

    ✅♻️ add tests for category controller

--- src/test/java/br/rocha/estocai/Category/CategoryControllerTest.java
+++ src/test/java/br/rocha/estocai/Category/CategoryControllerTest.java
@@ -10,15 +10,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
